828edadd61d888f864f8a31ef0983ce6d9a6abba
Zhang Yaxin <valses@sina.cn>
2016-08-01 17:55:48 +0800

    Array

---
+++ Find-the-Duplicate-Number/Find the Duplicate Number.c
@@ -0,0 +1,21 @@
+//
+//Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive), prove that at least one duplicate number must exist. Assume that there is only one duplicate number, find the duplicate one.
+//
+//Note:
+//    You must not modify the array (assume the array is read only).
+//    You must use only constant, O(1) extra space.
+//    Your runtime complexity should be less than O(n2).
+//    There is only one duplicate number in the array, but it could be repeated more than once.
+//
+//
+// 
+//
+int findDuplicate(int* nums, int numsSize) {
+    int i;
+    int x = 0;
+    for(i = 0;i < numsSize-1;i++){
+        x = x^(i+1)^*(nums+i);
+    }
+    x = x^*(nums+numsSize-1);
+    return x;
+}
